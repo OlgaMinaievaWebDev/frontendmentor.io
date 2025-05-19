6abdca8f105d6f55fb57dfa19de13b3020c99b89
Olga <minaeva9@gmail.com>
2025-05-19 15:46:24 -0400

    styled

--- qr-code-component/src/App.jsx
+++ qr-code-component/src/App.jsx
@@ -2,7 +2,7 @@ import QRCard from "./QRCard";
 
 function App() {
   return (
-    <div className="bg-blue-100 h-screen">
+    <div className="bg-blue-100 h-screen flex items-center justify-center">
      <QRCard/>
     </div>
   );
--- qr-code-component/src/QRCard.jsx
+++ qr-code-component/src/QRCard.jsx
@@ -1,7 +1,20 @@
+import QRImg from '../src/images/image-qr-code.png'
+
 function QRCard() {
  return (
-  <div className="w-[350px] rounded-2xl p-4 shadow-lg bg-white-100">
- 
+   <div className="w-[300px] rounded-2xl p-4 pb-8 shadow-lg bg-white-100">
+     <div className="flex flex-col items-center gap-5 text-center">
+       <img src={QRImg} alt="QR code image" className="w-full rounded-xl" />
+       <div className="px-4 space-y-4">
+         <h1 className="text-xl font-bold text-slate-900 leading-tight">
+           Improve your front-end skills by building projects
+         </h1>
+         <p className="text-md text-slate-500 px-2 mb-2">
+           Scan Qr code to visit Frontend Mentor and take your coding skills to
+           next level
+         </p>
+       </div>
+     </div>
    </div>
  );
 }

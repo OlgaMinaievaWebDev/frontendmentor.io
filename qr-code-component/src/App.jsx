9eee3ea6c9fb931ad0ed18c2faa8c2a6591be232
Olga <minaeva9@gmail.com>
2025-05-19 15:23:00 -0400

    styles added to css

--- qr-code-component/src/App.jsx
+++ qr-code-component/src/App.jsx
@@ -1,7 +1,9 @@
+import QRCard from "./QRCard";
+
 function App() {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div className="bg-blue-100 h-screen">
+     <QRCard/>
     </div>
   );
 }
---
+++ qr-code-component/src/QRCard.jsx
@@ -0,0 +1,9 @@
+function QRCard() {
+ return (
+  <div className="w-[350px] rounded-2xl p-4 shadow-lg bg-white-100">
+ 
+   </div>
+ );
+}
+
+export default QRCard

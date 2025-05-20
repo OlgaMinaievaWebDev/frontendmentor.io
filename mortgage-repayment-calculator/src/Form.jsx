b8b41d0a0bb6b8fc617c7a8ad9ca9bc65646814c
Olga <minaeva9@gmail.com>
2025-05-20 15:04:07 -0400

    image section styled

--- mortgage-repayment-calculator/src/Form.jsx
+++ mortgage-repayment-calculator/src/Form.jsx
@@ -6,33 +6,36 @@ function Form() {
    <form className="flex flex-col gap-4 items-start w-full text-slate-500">
      <button type="button" className="underline">
        Clear All
-   </button>
+     </button>
 
-   {/* Input Elements */}
-   <InputElement
-    label="Mortgage Amount"
-    id="amount"
-    name="amount"
-    type="number"
-    icon={<DollarSign size={16} />}
-    iconPosition="left" />
-   
-   <InputElement 
-      label="Mortgage Term"
-      id="term"
-      name="term"
-      type="number"
-      icon="years"
-    iconPosition="right" />
-   
-   <InputElement 
-     label="Interest Rate"
-     id="percent"
-     name="percent"
-     type="number"
-     icon="%"
-     iconPosition="right" />
-    
+     {/* Input Elements */}
+     <InputElement
+       label="Mortgage Amount"
+       id="amount"
+       name="amount"
+       type="number"
+       icon={<DollarSign size={16} />}
+       iconPosition="left"
+     />
+
+     <InputElement
+       label="Mortgage Term"
+       id="term"
+       name="term"
+       type="number"
+       icon="years"
+       iconPosition="right"
+     />
+
+     <InputElement
+       label="Interest Rate"
+       id="percent"
+       name="percent"
+       type="number"
+       icon="%"
+       iconPosition="right"
+     />
+
      <fieldset>
        <legend>Mortgage Type</legend>
        <label htmlFor="repayment">
@@ -44,7 +47,16 @@ function Form() {
      </fieldset>
 
      <button type="submit">Calculate Repayments</button>
-     <img src={image} alt="calculation illustration" />
+     <div className="bg-slate-800 flex flex-col items-center w-full p-6 gap-4">
+       <img src={image} alt="calculation illustration" className="max-w-xs" />
+       <h2 className="text-center text-white text-2xl font-semibold">
+         Results shown here
+       </h2>
+       <p className="text-slate-400 text-center text-lg max-w-md">
+         Complete the form and click 'calculate repayments' to see what your
+         monthly repayments would be
+       </p>
+     </div>
    </form>
  );
 }

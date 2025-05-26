938961b6edc8c8a093713c9d9cad4e45958e0cdc
Olga <minaeva9@gmail.com>
2025-05-25 23:41:11 -0400

    updated result for md

--- result-summary-component/src/components/Result.jsx
+++ result-summary-component/src/components/Result.jsx
@@ -1,6 +1,6 @@
 function Result() {
  return (
-  <div className="w-full md:w-1/2 bg-gradient-to-b from-indigo-600 to-indigo-800 text-white text-center p-12 rounded-3xl md:rounded-3xl flex flex-col items-center justify-center gap-4 ">
+  <div className="w-full md:w-1/2 bg-gradient-to-b from-indigo-600 to-indigo-800 text-white text-center p-12 md:rounded-3xl flex flex-col items-center justify-center gap-4 rounded-b-3xl">
    <h1 className="text-2xl font-semibold text-indigo-300">Your Result</h1>
    <div className="rounded-full w-44 h-44 bg-gradient-to-b from-indigo-800 to-indigo-600 shadow flex flex-col items-center justify-center gap-2 m-4">
     <p className="text-6xl font-extrabold">76</p>
--- result-summary-component/src/components/Summary.jsx
+++ result-summary-component/src/components/Summary.jsx
@@ -5,7 +5,7 @@ function Summary() {
    <div className="w-full md:w-1/2 flex flex-col items-start justify-center p-12 gap-4">
      <h1 className="text-2xl font-semibold text-blue-950">Summary</h1>
      <List />
-     <button className="bg-blue-950 w-full text-white text-xl py-2 px-20 rounded-4xl cursor-pointer hover:bg-blue-800 transition-colors duration-300">
+     <button className="bg-blue-950 w-full text-white text-xl py-3 px-20 rounded-4xl cursor-pointer hover:bg-blue-800 transition-colors duration-300">
        Continue
      </button>
    </div>

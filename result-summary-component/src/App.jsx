e5ceabb4e2396dbd080ddba4f99e9c5afb24e8d0
Olga <minaeva9@gmail.com>
2025-05-25 23:01:58 -0400

    result section

--- result-summary-component/src/App.jsx
+++ result-summary-component/src/App.jsx
@@ -3,8 +3,8 @@ import Summary from "./components/Summary";
 
 function App() {
   return (
-    <div className="min-h-screen bg-blue-100 flex items-center justify-center p-6">
-      <div className="flex flex-col md:flex-row md:rounded-3xl shadow-xl bg-white w-full  max-w-3xl">
+    <div className="min-h-screen bg-gradient-to-b from-blue-100 to-white flex items-center justify-center p-6">
+      <div className="flex flex-col md:flex-row bg-white rounded-3xl overflow-hidden shadow-xl max-w-3xl w-full">
         <Result />
         <Summary />
       </div>
--- result-summary-component/src/components/Result.jsx
+++ result-summary-component/src/components/Result.jsx
@@ -1,7 +1,13 @@
 function Result() {
  return (
-  <div className=" bg-red-300">
-   result
+  <div className="w-full md:w-1/2 bg-gradient-to-b from-indigo-600 to-indigo-800 text-white text-center p-12 rounded-3xl md:rounded-3xl flex flex-col items-center justify-center gap-4 ">
+   <h1 className="text-2xl font-semibold text-indigo-300">Your Result</h1>
+   <div className="rounded-full w-44 h-44 bg-gradient-to-b from-indigo-800 to-indigo-600 shadow flex flex-col items-center justify-center gap-2 m-4">
+    <p className="text-6xl font-extrabold">76</p>
+    <p className=" text-indigo-300"> of 100</p>
+   </div>
+    <p className="text-3xl font-bold">Great</p>
+    <p className="text-lg">You scored higher than 65% of the people who have taken these tests</p>
   </div>
  )
 }

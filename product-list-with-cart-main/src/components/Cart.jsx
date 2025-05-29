933e3815e7451c8728815e60b06647caa8e1b401
Olga <minaeva9@gmail.com>
2025-05-29 12:41:49 -0400

    cartItem added

--- product-list-with-cart-main/src/components/Cart.jsx
+++ product-list-with-cart-main/src/components/Cart.jsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import emptyCartImage from '../assets/images/illustration-empty-cart.svg'
 import CartContext from '../context/CartContext';
-
+import CartItem from './CartItem';
 
 function Cart() {
   const { state, dispatch } = useContext(CartContext);
@@ -25,25 +25,9 @@ function Cart() {
       </h2>
 
       <ul className="flex flex-col gap-4">
-        {/* Item 1 */}
+    
         {cartItems.map((item, index) => (
-          <li key={index}>
-            <div className="flex justify-between items-start border-b border-slate-200 pb-2">
-              <div>
-                <p className="font-semibold text-sm">{item.name}</p>
-                <div className="flex items-center gap-4 text-sm mt-1">
-                  <p className="text-amber-800 font-semibold">
-                    {item.quantity}x
-                  </p>
-                  <p>{item.price.toFixed(2)}</p>
-                  <p>{(item.price * item.quantity).toFixed(2)}</p>
-                </div>
-              </div>
-              <button className="w-5 h-5 rounded-full border border-slate-500 flex justify-center items-center hover:font-bold text-xs">
-                X
-              </button>
-            </div>
-          </li>
+          <CartItem key={ index} item={item} /> 
         ))}
 
         {/* Total */}
--- product-list-with-cart-main/src/components/CartItem.jsx
+++ product-list-with-cart-main/src/components/CartItem.jsx
@@ -1,9 +1,23 @@
-function CartItem() {
+function CartItem({index, item}) {
  return (
-  <div>
-   Cart Item
-  </div>
- )
+   <>
+     <li key={index}>
+       <div className="flex justify-between items-start border-b border-slate-200 pb-2">
+         <div>
+           <p className="font-semibold text-sm">{item.name}</p>
+           <div className="flex items-center gap-4 text-sm mt-1">
+             <p className="text-amber-800 font-semibold">{item.quantity}x</p>
+             <p>{item.price.toFixed(2)}</p>
+             <p>{(item.price * item.quantity).toFixed(2)}</p>
+           </div>
+         </div>
+         <button className="w-5 h-5 rounded-full border border-slate-500 flex justify-center items-center hover:font-bold text-xs">
+           X
+         </button>
+       </div>
+     </li>
+   </>
+ );
 }
 
 export default CartItem

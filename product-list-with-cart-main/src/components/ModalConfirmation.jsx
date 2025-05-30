8e102784490d3fdf0e516c86654dc2ef9eb51105
Olga <minaeva9@gmail.com>
2025-05-30 15:43:18 -0400

    modal styled

--- product-list-with-cart-main/src/components/ModalConfirmation.jsx
+++ product-list-with-cart-main/src/components/ModalConfirmation.jsx
@@ -19,26 +19,29 @@ function ModalConfirmation({
     setOrderConfirmed(false);
   };
 
-  return (
-    <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
-      <img
-        src={iconOrderConfirmed}
-        alt="Order Confirmed"
-        className="w-12 h-12 mb-4"
-      />
-      <h3 className="text-lg font-bold mb-4">Order Confirmed</h3>
-      <p className="mb-4">We hope you enjoy your food</p>
+ return (
+   <div className="fixed inset-0 flex items-center justify-center bg-slate-400/60 z-50">
+     <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
+       <img
+         src={iconOrderConfirmed}
+         alt="Order Confirmed"
+         className="w-12 h-12 mb-4"
+       />
+       <h3 className="text-4xl font-bold">Order </h3>
+       <h3 className="text-4xl font-bold mb-2">Confirmed </h3>
+       <p className="mb-4 text-slate-400">We hope you enjoy your food</p>
 
-      <Cart showTitle={false} showNote={false} />
+       <Cart showTitle={false} showNote={false} />
 
-      <button
-        onClick={handleStartNewOrder}
-        className="bg-orange-700 text-white font-medium rounded-full px-6 py-2 hover:bg-orange-800 transition text-sm md:text-base mt-4 w-full "
-      >
-        Start New Order
-      </button>
-    </div>
-  );
+       <button
+         onClick={handleStartNewOrder}
+         className="bg-orange-700 text-white font-medium rounded-full px-6 py-2 hover:bg-orange-800 transition text-sm md:text-base mt-8 w-full "
+       >
+         Start New Order
+       </button>
+     </div>
+   </div>
+ );
 }
 
 export default ModalConfirmation;

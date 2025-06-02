142568d69b95899dfef407e352eba86922fe0e6a
Olga <minaeva9@gmail.com>
2025-06-02 15:00:03 -0400

    Total added

--- expenses-chart-component/src/components/Card.jsx
+++ expenses-chart-component/src/components/Card.jsx
@@ -1,12 +1,12 @@
 import Bar from "./BarChart"
+import Total from "./Total"
 
 function Card() {
  return (
   <div className="flex flex-col gap-4 bg-white rounded-2xl p-6 shadow-lg">
     <h2 className="text-xl font-semibold">Spending - Last 7 days</h2>
-   
-    {/* Add more content or components as needed */}
-  <Bar/>
+   <Bar />
+   <Total/>
   </div>
  )
 }
---
+++ expenses-chart-component/src/components/Total.jsx
@@ -0,0 +1,16 @@
+function Total() {
+ return (
+   <div className='flex justify-between items-center mt-3'>
+    <div className='flex flex-col'>
+    <p className='text-sm font-extralight text-brown-light '>Total this month</p>
+    <p className="font-bold text-2xl">$921.48</p>
+    </div>
+   <div className="flex flex-col items-end justify-end">
+    <p className="font-bold text-xs">+2.4%</p>
+    <p className="text-xs text-brown-light">from last month</p>
+  </div>
+   </div>
+ )
+}
+
+export default Total
